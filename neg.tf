c1c6786e95a102829a6d9f5d5c8526747cde8b95
Nikita Gour <145796723+nikki152@users.noreply.github.com>
2026-01-13 17:43:30 +0530

    Update neg.tf

--- neg.tf
+++ neg.tf
@@ -1,10 +1,10 @@
-resource "google_compute_region_network_endpoint_group" "cr_neg" {
+resource "google_compute_region_network_endpoint_group" "cloudrun_neg" {
   name                  = "cloudrun-neg"
   region                = var.region
   network_endpoint_type = "SERVERLESS"
  
   cloud_run {
-    service = data.google_cloud_run_service.cloudrun.name
+    service = data.google_cloud_run_service.existing.name
   }
 }
  
