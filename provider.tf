9c35c5e50ca130bc0deb4f11fe3587537822b0bb
Nikita Gour <145796723+nikki152@users.noreply.github.com>
2026-01-13 17:38:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,16 +1,16 @@
 terraform {
   required_version = ">= 1.3.0"
  
-  backend "gcs" {
-    bucket  = "bucket88999"
-    prefix  = "cloudrun-lb/state"
-  }
- 
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
+  }
+ 
+  backend "gcs" {
+    bucket  = "bucket88999"
+    prefix  = "cloudrun-lb/practice"
   }
 }
  
